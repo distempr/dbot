8fc8aced5ae871da1797ff88fae0d06431ac1d39
Harry Wheildon <harry.wheildon@gmail.com>
2023-11-24 17:15:07 +0000

    Added ChatGPT integration

--- bot.py
+++ bot.py
@@ -4,18 +4,42 @@ import tomllib
 
 import boto3
 
+from openai import OpenAI
 from telegram import Update
-from telegram.ext import Application, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, MessageHandler, filters
+
+
+CHAT_SYSTEM_PROMPT = 'You are my personal bot. You prefer brief, to-the-point answers and help me with stupid questions.'
+CHAT_TEMPERATURE = 1
+CHAT_MODEL = 'gpt-4'
 
 
 with open('bot.toml', 'rb') as f:
     config = tomllib.load(f)
+
+
+chat_client = OpenAI(api_key=config['openai']['api_key'])
 
 session = boto3.Session(profile_name='ec2-basics')
 ec2 = session.resource('ec2', region_name=config['aws']['region'])
 
 con = sqlite3.connect('bot.db')
 cur = con.cursor()
+
+
+def chat_completion(prompt):
+    messages = [
+        {'role': 'system', 'content': CHAT_SYSTEM_PROMPT},
+        {'role': 'user', 'content': prompt}
+    ]
+
+    response = chat_client.chat.completions.create(
+        model=CHAT_MODEL,
+        messages=messages,
+        temperature=CHAT_TEMPERATURE
+    )
+
+    return response.choices[0].message.content
 
 
 def get_ec2_instance_state(instance_id):
@@ -32,8 +56,17 @@ async def send_message(context, text):
     await context.bot.send_message(chat_id=user_id, text=text)
 
 
+async def chat(update, context):
+    user_id = config['tg']['my_user_id']
+    if update.message.from_user['id'] != user_id:
+        print('Message not from myself')
+        return None
+
+    response = chat_completion(update.message.text)
+    await update.message.reply_text(response)
+
+
 async def ec2(context):
-    print('hello world')
     # run some code and then if conditions are met...
     condition = False
     if condition:
@@ -48,6 +81,8 @@ async def du(context):
 
 def main():
     application = Application.builder().token(config['tg']['token']).build()
+
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
 
     application.job_queue.run_repeating(ec2, 60)
     application.job_queue.run_repeating(du, config['du']['notify_every'])
